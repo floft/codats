486fa06204abea61de8cdb7ca7b31be581077eb6
Garrett Wilson <g@floft.net>
2019-07-12 11:22:59 -0700

    walking vs. running synthetic data

--- datasets/generate_trivial_datasets.py
+++ datasets/generate_trivial_datasets.py
@@ -77,7 +77,7 @@ def generate_positive_slope_data(n, display=False, add_noise=False,
     return to_pandas(y, labels)
 
 
-def sine(m=1.0, b=0.0, f=None, freq_noise=1.0, phase_shift=5.0,
+def sine(m=1.0, b=0.0, f=None, amps=None, freq_noise=1.0, phase_shift=5.0,
         length=100, mint=0, maxt=2):
     """
     Generate a single or multiple sine waves (multiple if f is list)
@@ -85,6 +85,7 @@ def sine(m=1.0, b=0.0, f=None, freq_noise=1.0, phase_shift=5.0,
     m - scale horizontally
     b - offset vertically
     f - either one frequency, a list of frequencies for each example (see below)
+    amps - if specified, the amplitude of each specified frequency
     freq_noise - if not None, frequencies noisy about what is given in f
     phase_shift - how much to randomly offset in time (per frequency, so
         even with freq_noise=None, the two samples of f=[[1,2], [1,2]] will
@@ -107,7 +108,13 @@ def sine(m=1.0, b=0.0, f=None, freq_noise=1.0, phase_shift=5.0,
     # Set frequency if desired
     if f is None:
         s = np.array(1.0, dtype=np.float32)
+        amps = np.array(1.0, dtype=np.float32)
     else:
+        if amps is not None:
+            amps = np.array(amps, dtype=np.float32)
+        else:
+            amps = np.array([1.0]*len(f), dtype=np.float32)
+
         f = np.array(f, dtype=np.float32)
 
         if freq_noise is not None:
@@ -140,12 +147,17 @@ def sine(m=1.0, b=0.0, f=None, freq_noise=1.0, phase_shift=5.0,
 
         phase_shift = np.random.normal(0.0, phase_shift, shape)
 
-    y = m*np.sin(s*(x+phase_shift)) + b
+    y = m*amps*np.sin(s*(x+phase_shift))
 
     # Sum the extra dimension for multiple frequencies, e.g. from (100,3,2)
     # back to (100,3) if 2 frequencies each
+    #
+    # Note: make sure we don't add b in y above before summing, since otherwise
+    # it'll be shifted much more than b
     if multi_freq:
-        y = np.sum(y, axis=2)
+        y = np.sum(y, axis=2) + b
+    else:
+        y += b
 
     return x_orig, y
 
@@ -190,30 +202,65 @@ def generate_freq(n, display=False, amp_noise=False, freq_noise=False,
     return to_pandas(y, labels)
 
 
-def generate_multi_freq(n, pos_f, neg_f, display=False,
+def generate_multi_freq(n, pos_f, neg_f, pos_amp=None, neg_amp=None,
+        display=False,
         amp_noise=0.1, freq_noise=1.0, phase_shift=5.0,
-        sample_freq=50):
+        sample_freq=50, duration=2, b=0.0):
     """
     Generate data with different sets of frequencies for +/- classes
+
+    Optionally specify different amplitudes for each of the frequencies. If not,
+    then they all have the same amplitude (possibly with amplitude noise if
+    amp_noise is not None).
     """
+    assert pos_amp is None or len(pos_amp) == len(pos_f), \
+        "pos_amp must be same length as pos_f"
+    assert neg_amp is None or len(neg_amp) == len(neg_f), \
+        "neg_amp must be same length as neg_f"
+
+    if pos_amp is not None:
+        pos_amp = np.array(pos_amp, dtype=np.float32)
+    else:
+        pos_amp = np.array([1.0]*len(pos_f), dtype=np.float32)
+
+    if neg_amp is not None:
+        neg_amp = np.array(neg_amp, dtype=np.float32)
+    else:
+        neg_amp = np.array([1.0]*len(neg_f), dtype=np.float32)
+
     pos_f = np.array(pos_f, dtype=np.float32)
     neg_f = np.array(neg_f, dtype=np.float32)
+
+    # Match sizes by zero padding, otherwise we can't convert to single matrix
+    if len(pos_f) > len(neg_f):
+        padding = len(pos_f) - len(neg_f)
+        neg_f = np.pad(neg_f, (0, padding), 'constant', constant_values=(0.0, 0.0))
+        neg_amp = np.pad(neg_amp, (0, padding), 'constant', constant_values=(0.0, 0.0))
+    elif len(neg_f) > len(pos_f):
+        padding = len(neg_f) - len(pos_f)
+        pos_f = np.pad(pos_f, (0, padding), 'constant', constant_values=(0.0, 0.0))
+        pos_amp = np.pad(pos_amp, (0, padding), 'constant', constant_values=(0.0, 0.0))
 
     # Generate the labels, ~1/2 from + and 1/2 from - classes
     labels = np.random.randint(2, size=n)
 
     # Get approximately the pos_f/neg_f frequencies for each
     freqs = []
+    amps = []
 
     for label in labels:
         f = neg_f if label == 0 else pos_f
+        amp = neg_amp if label == 0 else pos_amp
         freqs.append(f)
+        amps.append(amp)
 
     freqs = np.array(freqs, dtype=np.float32)
+    amps = np.array(amps, dtype=np.float32)
+    amps[np.isnan(amps)] = 1.0  # None is NaN, and so just set to 1.0 amplitude
 
     # Generate time series data
-    x, y = sine(f=freqs, maxt=2, length=2*sample_freq, freq_noise=freq_noise,
-        phase_shift=phase_shift)
+    x, y = sine(b=b, f=freqs, amps=amps, maxt=duration, length=duration*sample_freq,
+        freq_noise=freq_noise, phase_shift=phase_shift)
 
     if amp_noise is not None:
         y += np.random.normal(0.0, amp_noise, (y.shape[0], n))
@@ -269,43 +316,106 @@ if __name__ == '__main__':
     # save_data(lambda x, dsp: generate_multi_freq(x, [2, 6, 8], [7, 9, 11], display=dsp), 'freqscale_low', dsp)
     # save_data(lambda x, dsp: generate_multi_freq(x, [4, 12, 16], [14, 18, 22], display=dsp), 'freqscale_high', dsp)
 
+    # #
+    # # Invertible (for directly evaluating mapping)
+    # #
+    # # jumping mean - 1 with some overlap, 2 with no overlap
+    # np.random.seed(0)
+    # save_data(lambda x, dsp: generate_positive_slope_data(x, bmin=0.0, bmax=5.0, display=dsp), 'line1low', dsp)
+    # save_data(lambda x, dsp: generate_positive_slope_data(x, bmin=2.5, bmax=7.5, display=dsp), 'line1high', dsp)
+    # save_data(lambda x, dsp: generate_positive_slope_data(x, bmin=0.0, bmax=5.0, display=dsp), 'line2low', dsp)
+    # save_data(lambda x, dsp: generate_positive_slope_data(x, bmin=5.0, bmax=10.0, display=dsp), 'line2high', dsp)
+
+    # save_data(lambda x, dsp: generate_positive_sine_data(x, bmin=0.0, bmax=5.0, display=dsp), 'sine1low', dsp)
+    # save_data(lambda x, dsp: generate_positive_sine_data(x, bmin=2.5, bmax=7.5, display=dsp), 'sine1high', dsp)
+    # save_data(lambda x, dsp: generate_positive_sine_data(x, bmin=0.0, bmax=5.0, display=dsp), 'sine2low', dsp)
+    # save_data(lambda x, dsp: generate_positive_sine_data(x, bmin=5.0, bmax=10.0, display=dsp), 'sine2high', dsp)
+
+    # # see if network works better with higher-frequency data
+    # save_data(lambda x, dsp: generate_positive_sine_data(x, bmin=0.0, bmax=5.0, f=10.0, display=dsp), 'sine3low', dsp)
+    # save_data(lambda x, dsp: generate_positive_sine_data(x, bmin=2.5, bmax=7.5, f=10.0, display=dsp), 'sine3high', dsp)
+    # save_data(lambda x, dsp: generate_positive_sine_data(x, bmin=0.0, bmax=5.0, f=10.0, display=dsp), 'sine4low', dsp)
+    # save_data(lambda x, dsp: generate_positive_sine_data(x, bmin=5.0, bmax=10.0, f=10.0, display=dsp), 'sine4high', dsp)
+
+    # # slope scaling - 1 with small scaling, 2 with more scaling
+    # np.random.seed(0)
+    # save_data(lambda x, dsp: generate_positive_slope_data(x, mmin=-1.0, mmax=1.0, display=dsp), 'lineslope1low', dsp)
+    # save_data(lambda x, dsp: generate_positive_slope_data(x, mmin=-1.5, mmax=1.5, display=dsp), 'lineslope1high', dsp)
+    # save_data(lambda x, dsp: generate_positive_slope_data(x, mmin=-1.0, mmax=1.0, display=dsp), 'lineslope2low', dsp)
+    # save_data(lambda x, dsp: generate_positive_slope_data(x, mmin=-2.0, mmax=2.0, display=dsp), 'lineslope2high', dsp)
+
+    # save_data(lambda x, dsp: generate_positive_sine_data(x, mmin=-1.0, mmax=1.0, display=dsp), 'sineslope1low', dsp)
+    # save_data(lambda x, dsp: generate_positive_sine_data(x, mmin=-1.5, mmax=1.5, display=dsp), 'sineslope1high', dsp)
+    # save_data(lambda x, dsp: generate_positive_sine_data(x, mmin=-1.0, mmax=1.0, display=dsp), 'sineslope2low', dsp)
+    # save_data(lambda x, dsp: generate_positive_sine_data(x, mmin=-2.0, mmax=2.0, display=dsp), 'sineslope2high', dsp)
+
+    # save_data(lambda x, dsp: generate_positive_sine_data(x, mmin=-1.0, mmax=1.0, f=10.0, display=dsp), 'sineslope3low', dsp)
+    # save_data(lambda x, dsp: generate_positive_sine_data(x, mmin=-1.5, mmax=1.5, f=10.0, display=dsp), 'sineslope3high', dsp)
+    # save_data(lambda x, dsp: generate_positive_sine_data(x, mmin=-1.0, mmax=1.0, f=10.0, display=dsp), 'sineslope4low', dsp)
+    # save_data(lambda x, dsp: generate_positive_sine_data(x, mmin=-2.0, mmax=2.0, f=10.0, display=dsp), 'sineslope4high', dsp)
+
     #
-    # Invertible (for directly evaluating mapping)
+    # Classification problem:
+    # Walking (negative) vs. running (positive)
     #
-    # jumping mean - 1 with some overlap, 2 with no overlap
+    run_f = np.array([1, 2, 3, 4, 5, 6], dtype=np.float32)
+    run_amp = np.array([0.75, 0.56, 0.38, 0.19, 0.08, 0.04], dtype=np.float32)
+    walk_f = np.array([1, 2, 4], dtype=np.float32)
+    walk_amp = np.array([0.5, 0.25, 0.06], dtype=np.float32)
+
+    # Frequency shift
     np.random.seed(0)
-    save_data(lambda x, dsp: generate_positive_slope_data(x, bmin=0.0, bmax=5.0, display=dsp), 'line1low', dsp)
-    save_data(lambda x, dsp: generate_positive_slope_data(x, bmin=2.5, bmax=7.5, display=dsp), 'line1high', dsp)
-    save_data(lambda x, dsp: generate_positive_slope_data(x, bmin=0.0, bmax=5.0, display=dsp), 'line2low', dsp)
-    save_data(lambda x, dsp: generate_positive_slope_data(x, bmin=5.0, bmax=10.0, display=dsp), 'line2high', dsp)
+    save_data(lambda x, dsp: generate_multi_freq(x, run_f, walk_f, run_amp, walk_amp, dsp, None, None, None), 'freqshift_a', dsp)
+    save_data(lambda x, dsp: generate_multi_freq(x, run_f+0.0, walk_f+0.0, run_amp, walk_amp, dsp, None, None, None), 'freqshift_b0', dsp)
+    save_data(lambda x, dsp: generate_multi_freq(x, run_f+3.8, walk_f+3.8, run_amp, walk_amp, dsp, None, None, None), 'freqshift_b1', dsp)
+    save_data(lambda x, dsp: generate_multi_freq(x, run_f+7.6, walk_f+7.6, run_amp, walk_amp, dsp, None, None, None), 'freqshift_b2', dsp)
+    save_data(lambda x, dsp: generate_multi_freq(x, run_f+11.4, walk_f+11.4, run_amp, walk_amp, dsp, None, None, None), 'freqshift_b3', dsp)
+    save_data(lambda x, dsp: generate_multi_freq(x, run_f+15.2, walk_f+15.2, run_amp, walk_amp, dsp, None, None, None), 'freqshift_b4', dsp)
+    save_data(lambda x, dsp: generate_multi_freq(x, run_f+19.0, walk_f+19.0, run_amp, walk_amp, dsp, None, None, None), 'freqshift_b5', dsp)
 
-    save_data(lambda x, dsp: generate_positive_sine_data(x, bmin=0.0, bmax=5.0, display=dsp), 'sine1low', dsp)
-    save_data(lambda x, dsp: generate_positive_sine_data(x, bmin=2.5, bmax=7.5, display=dsp), 'sine1high', dsp)
-    save_data(lambda x, dsp: generate_positive_sine_data(x, bmin=0.0, bmax=5.0, display=dsp), 'sine2low', dsp)
-    save_data(lambda x, dsp: generate_positive_sine_data(x, bmin=5.0, bmax=10.0, display=dsp), 'sine2high', dsp)
+    save_data(lambda x, dsp: generate_multi_freq(x, run_f, walk_f, run_amp, walk_amp, dsp, None, None), 'freqshift_phase_a', dsp)
+    save_data(lambda x, dsp: generate_multi_freq(x, run_f+0.0, walk_f+0.0, run_amp, walk_amp, dsp, None, None), 'freqshift_phase_b0', dsp)
+    save_data(lambda x, dsp: generate_multi_freq(x, run_f+3.8, walk_f+3.8, run_amp, walk_amp, dsp, None, None), 'freqshift_phase_b1', dsp)
+    save_data(lambda x, dsp: generate_multi_freq(x, run_f+7.6, walk_f+7.6, run_amp, walk_amp, dsp, None, None), 'freqshift_phase_b2', dsp)
+    save_data(lambda x, dsp: generate_multi_freq(x, run_f+11.4, walk_f+11.4, run_amp, walk_amp, dsp, None, None), 'freqshift_phase_b3', dsp)
+    save_data(lambda x, dsp: generate_multi_freq(x, run_f+15.2, walk_f+15.2, run_amp, walk_amp, dsp, None, None), 'freqshift_phase_b4', dsp)
+    save_data(lambda x, dsp: generate_multi_freq(x, run_f+19.0, walk_f+19.0, run_amp, walk_amp, dsp, None, None), 'freqshift_phase_b5', dsp)
 
-    # see if network works better with higher-frequency data
-    save_data(lambda x, dsp: generate_positive_sine_data(x, bmin=0.0, bmax=5.0, f=10.0, display=dsp), 'sine3low', dsp)
-    save_data(lambda x, dsp: generate_positive_sine_data(x, bmin=2.5, bmax=7.5, f=10.0, display=dsp), 'sine3high', dsp)
-    save_data(lambda x, dsp: generate_positive_sine_data(x, bmin=0.0, bmax=5.0, f=10.0, display=dsp), 'sine4low', dsp)
-    save_data(lambda x, dsp: generate_positive_sine_data(x, bmin=5.0, bmax=10.0, f=10.0, display=dsp), 'sine4high', dsp)
-
-    # slope scaling - 1 with small scaling, 2 with more scaling
+    # Frequency scale (shift looks identical, but scaled horizontally)
     np.random.seed(0)
-    save_data(lambda x, dsp: generate_positive_slope_data(x, mmin=-1.0, mmax=1.0, display=dsp), 'lineslope1low', dsp)
-    save_data(lambda x, dsp: generate_positive_slope_data(x, mmin=-1.5, mmax=1.5, display=dsp), 'lineslope1high', dsp)
-    save_data(lambda x, dsp: generate_positive_slope_data(x, mmin=-1.0, mmax=1.0, display=dsp), 'lineslope2low', dsp)
-    save_data(lambda x, dsp: generate_positive_slope_data(x, mmin=-2.0, mmax=2.0, display=dsp), 'lineslope2high', dsp)
+    save_data(lambda x, dsp: generate_multi_freq(x, run_f, walk_f, run_amp, walk_amp, dsp, None, None, None), 'freqscale_a', dsp)
+    save_data(lambda x, dsp: generate_multi_freq(x, run_f*1.000, walk_f*1.000, run_amp, walk_amp, dsp, None, None, None), 'freqscale_b0', dsp)
+    save_data(lambda x, dsp: generate_multi_freq(x, run_f*1.633, walk_f*1.633, run_amp, walk_amp, dsp, None, None, None), 'freqscale_b1', dsp)
+    save_data(lambda x, dsp: generate_multi_freq(x, run_f*2.266, walk_f*2.266, run_amp, walk_amp, dsp, None, None, None), 'freqscale_b2', dsp)
+    save_data(lambda x, dsp: generate_multi_freq(x, run_f*2.900, walk_f*2.900, run_amp, walk_amp, dsp, None, None, None), 'freqscale_b3', dsp)
+    save_data(lambda x, dsp: generate_multi_freq(x, run_f*3.533, walk_f*3.533, run_amp, walk_amp, dsp, None, None, None), 'freqscale_b4', dsp)
+    save_data(lambda x, dsp: generate_multi_freq(x, run_f*4.166, walk_f*4.166, run_amp, walk_amp, dsp, None, None, None), 'freqscale_b5', dsp)
 
-    save_data(lambda x, dsp: generate_positive_sine_data(x, mmin=-1.0, mmax=1.0, display=dsp), 'sineslope1low', dsp)
-    save_data(lambda x, dsp: generate_positive_sine_data(x, mmin=-1.5, mmax=1.5, display=dsp), 'sineslope1high', dsp)
-    save_data(lambda x, dsp: generate_positive_sine_data(x, mmin=-1.0, mmax=1.0, display=dsp), 'sineslope2low', dsp)
-    save_data(lambda x, dsp: generate_positive_sine_data(x, mmin=-2.0, mmax=2.0, display=dsp), 'sineslope2high', dsp)
+    save_data(lambda x, dsp: generate_multi_freq(x, run_f, walk_f, run_amp, walk_amp, dsp, None, None), 'freqscale_phase_a', dsp)
+    save_data(lambda x, dsp: generate_multi_freq(x, run_f*1.000, walk_f*1.000, run_amp, walk_amp, dsp, None, None), 'freqscale_phase_b0', dsp)
+    save_data(lambda x, dsp: generate_multi_freq(x, run_f*1.633, walk_f*1.633, run_amp, walk_amp, dsp, None, None), 'freqscale_phase_b1', dsp)
+    save_data(lambda x, dsp: generate_multi_freq(x, run_f*2.266, walk_f*2.266, run_amp, walk_amp, dsp, None, None), 'freqscale_phase_b2', dsp)
+    save_data(lambda x, dsp: generate_multi_freq(x, run_f*2.900, walk_f*2.900, run_amp, walk_amp, dsp, None, None), 'freqscale_phase_b3', dsp)
+    save_data(lambda x, dsp: generate_multi_freq(x, run_f*3.533, walk_f*3.533, run_amp, walk_amp, dsp, None, None), 'freqscale_phase_b4', dsp)
+    save_data(lambda x, dsp: generate_multi_freq(x, run_f*4.166, walk_f*4.166, run_amp, walk_amp, dsp, None, None), 'freqscale_phase_b5', dsp)
 
-    save_data(lambda x, dsp: generate_positive_sine_data(x, mmin=-1.0, mmax=1.0, f=10.0, display=dsp), 'sineslope3low', dsp)
-    save_data(lambda x, dsp: generate_positive_sine_data(x, mmin=-1.5, mmax=1.5, f=10.0, display=dsp), 'sineslope3high', dsp)
-    save_data(lambda x, dsp: generate_positive_sine_data(x, mmin=-1.0, mmax=1.0, f=10.0, display=dsp), 'sineslope4low', dsp)
-    save_data(lambda x, dsp: generate_positive_sine_data(x, mmin=-2.0, mmax=2.0, f=10.0, display=dsp), 'sineslope4high', dsp)
+    # Jumping mean
+    np.random.seed(0)
+    save_data(lambda x, dsp: generate_multi_freq(x, run_f, walk_f, run_amp, walk_amp, dsp, None, None, None, b=0.0), 'jumpmean_a', dsp)
+    save_data(lambda x, dsp: generate_multi_freq(x, run_f, walk_f, run_amp, walk_amp, dsp, None, None, None, b=0.0), 'jumpmean_b0', dsp)
+    save_data(lambda x, dsp: generate_multi_freq(x, run_f, walk_f, run_amp, walk_amp, dsp, None, None, None, b=2.0), 'jumpmean_b1', dsp)
+    save_data(lambda x, dsp: generate_multi_freq(x, run_f, walk_f, run_amp, walk_amp, dsp, None, None, None, b=4.0), 'jumpmean_b2', dsp)
+    save_data(lambda x, dsp: generate_multi_freq(x, run_f, walk_f, run_amp, walk_amp, dsp, None, None, None, b=8.0), 'jumpmean_b3', dsp)
+    save_data(lambda x, dsp: generate_multi_freq(x, run_f, walk_f, run_amp, walk_amp, dsp, None, None, None, b=16.0), 'jumpmean_b4', dsp)
+    save_data(lambda x, dsp: generate_multi_freq(x, run_f, walk_f, run_amp, walk_amp, dsp, None, None, None, b=32.0), 'jumpmean_b5', dsp)
+
+    save_data(lambda x, dsp: generate_multi_freq(x, run_f, walk_f, run_amp, walk_amp, dsp, None, None, b=0.0), 'jumpmean_phase_a', dsp)
+    save_data(lambda x, dsp: generate_multi_freq(x, run_f, walk_f, run_amp, walk_amp, dsp, None, None, b=0.0), 'jumpmean_phase_b0', dsp)
+    save_data(lambda x, dsp: generate_multi_freq(x, run_f, walk_f, run_amp, walk_amp, dsp, None, None, b=2.0), 'jumpmean_phase_b1', dsp)
+    save_data(lambda x, dsp: generate_multi_freq(x, run_f, walk_f, run_amp, walk_amp, dsp, None, None, b=4.0), 'jumpmean_phase_b2', dsp)
+    save_data(lambda x, dsp: generate_multi_freq(x, run_f, walk_f, run_amp, walk_amp, dsp, None, None, b=8.0), 'jumpmean_phase_b3', dsp)
+    save_data(lambda x, dsp: generate_multi_freq(x, run_f, walk_f, run_amp, walk_amp, dsp, None, None, b=16.0), 'jumpmean_phase_b4', dsp)
+    save_data(lambda x, dsp: generate_multi_freq(x, run_f, walk_f, run_amp, walk_amp, dsp, None, None, b=32.0), 'jumpmean_phase_b5', dsp)
 
     # Linear transform (random/fixed)
     # ...
