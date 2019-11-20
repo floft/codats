05462c37b4c43828f56c5d0ef392d9b4856e605f
Garrett Wilson <g@floft.net>
2019-11-20 08:26:11 -0800

    Overleaf had problems with filenames with spaces

--- copy_figures.sh
+++ copy_figures.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 target="/home/garrett/Documents/Github/multi-source-adaptation-paper/figures"
-cp results/multisource_average* "$target/"
+for file in results/multisource_average*.pdf; do
+    cp -a "$file" "$target/$(basename "${file// /_}")"
+done
