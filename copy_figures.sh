3786ee604afa68bfe6ff833abb396310840312b4
Garrett Wilson <g@floft.net>
2019-11-22 16:44:45 -0800

    copy avg figures

--- copy_figures.sh
+++ copy_figures.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 target="/home/garrett/Documents/Github/multi-source-adaptation-paper/figures"
-for file in results/multisource_average*.pdf; do
+for file in results/multisource_methodaverage*.pdf results/multisource_average*.pdf; do
     cp -a "$file" "$target/$(basename "${file// /_}")"
 done
