9dade9153570f0223082eec0e030df5fc9870ba1
Garrett Wilson <g@floft.net>
2020-01-22 10:11:59 -0800

    split normalization code

--- datasets/datasets.py
+++ datasets/datasets.py
@@ -19,99 +19,12 @@ from sklearn.model_selection import train_test_split
 from absl import app
 from absl import flags
 
+from normalization import calc_normalization, calc_normalization_jagged, \
+    apply_normalization, apply_normalization_jagged
+
 FLAGS = flags.FLAGS
 
 flags.DEFINE_enum("normalize", "meanstd", ["none", "minmax", "meanstd"], "How to normalize data")
-
-
-# For normalization
-def calc_normalization(x, method):
-    """
-    Calculate zero mean unit variance normalization statistics
-
-    We calculate separate mean/std or min/max statistics for each
-    feature/channel, the default (-1) for BatchNormalization in TensorFlow and
-    I think makes the most sense. If we set axis=0, then we end up with a
-    separate statistic for each time step and feature, and then we can get odd
-    jumps between time steps. Though, we get shape problems when setting axis=2
-    in numpy, so instead we reshape/transpose.
-    """
-    # from (10000,100,1) to (1,100,10000)
-    x = x.T
-    # from (1,100,10000) to (1,100*10000)
-    x = x.reshape((x.shape[0], -1))
-    # then we compute statistics over axis=1, i.e. along 100*10000 and end up
-    # with 1 statistic per channel (in this example only one)
-
-    if method == "meanstd":
-        values = (np.mean(x, axis=1), np.std(x, axis=1))
-    elif method == "minmax":
-        values = (np.min(x, axis=1), np.max(x, axis=1))
-    else:
-        raise NotImplementedError("unsupported normalization method")
-
-    return method, values
-
-
-def calc_normalization_jagged(x, method):
-    """ Same as calc_normalization() except works for arrays of varying-length
-    numpy arrays
-
-    x should be: [
-        np.array([example 1 time steps, example 1 features]),
-        np.array([example 2 time steps, example 2 features]),
-        ...
-    ] where the # time steps can differ between examples.
-    """
-    num_features = x[0].shape[1]
-    features = [None for x in range(num_features)]
-
-    for example in x:
-        transpose = example.T
-
-        for i, feature_values in enumerate(transpose):
-            if features[i] is None:
-                features[i] = feature_values
-            else:
-                features[i] = np.concatenate([features[i], feature_values], axis=0)
-
-    if method == "meanstd":
-        values = (np.array([np.mean(x) for x in features], dtype=np.float32),
-            np.array([np.std(x) for x in features], dtype=np.float32))
-    elif method == "minmax":
-        values = (np.array([np.min(x) for x in features], dtype=np.float32),
-            np.array([np.max(x) for x in features], dtype=np.float32))
-    else:
-        raise NotImplementedError("unsupported normalization method")
-
-    return method, values
-
-
-def apply_normalization(x, normalization, epsilon=1e-5):
-    """ Apply zero mean unit variance normalization statistics """
-    method, values = normalization
-
-    if method == "meanstd":
-        mean, std = values
-        x = (x - mean) / (std + epsilon)
-    elif method == "minmax":
-        minx, maxx = values
-        x = (x - minx) / (maxx - minx + epsilon) - 0.5
-
-    x[np.isnan(x)] = 0
-
-    return x
-
-
-def apply_normalization_jagged(x, normalization, epsilon=1e-5):
-    """ Same as apply_normalization() except works for arrays of varying-length
-    numpy arrays """
-    normalized = []
-
-    for example in x:
-        normalized.append(apply_normalization(example, normalization, epsilon))
-
-    return normalized
 
 
 class Dataset:
