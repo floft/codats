aba20df0dc4e11a1801748b9264d81d0ba5b51ea
Garrett Wilson <g@floft.net>
2020-04-27 16:42:31 -0700

    support disabling logs/plots/saves
    Needed since for heterogeneous DA I broke plotting

--- main.py
+++ main.py
@@ -31,10 +31,10 @@ flags.DEFINE_string("target", "", "What target domain to use (e.g. \"4\", can be
 flags.DEFINE_string("uid", None, "A unique ID saved in the log/model folder names to avoid conflicts")
 flags.DEFINE_integer("steps", 30000, "Number of training steps to run")
 flags.DEFINE_float("gpumem", 3350, "GPU memory to let TensorFlow use, in MiB (0 for all)")
-flags.DEFINE_integer("model_steps", 4000, "Save the model every so many steps")
-flags.DEFINE_integer("log_train_steps", 500, "Log training information every so many steps")
-flags.DEFINE_integer("log_val_steps", 4000, "Log validation information every so many steps (also saves model)")
-flags.DEFINE_integer("log_plots_steps", 4000, "Log plots every so many steps")
+flags.DEFINE_integer("model_steps", 0, "Save the model every so many steps (0 for only when log_val_steps)")
+flags.DEFINE_integer("log_train_steps", 500, "Log training information every so many steps (0 for never)")
+flags.DEFINE_integer("log_val_steps", 4000, "Log validation information every so many steps (also saves model, 0 for only at end)")
+flags.DEFINE_integer("log_plots_steps", 0, "Log plots every so many steps (0 for never)")
 flags.DEFINE_boolean("test", False, "Use real test set for evaluation rather than validation set")
 flags.DEFINE_boolean("subdir", True, "Save models/logs in subdirectory of prefix")
 flags.DEFINE_boolean("debug", False, "Start new log/model/images rather than continuing from previous run")
@@ -148,13 +148,14 @@ def main(argv):
             sys.stdout.flush()  # otherwise waits till the end to flush on Kamiak
 
         # Metrics on training/validation data
-        if i%FLAGS.log_train_steps == 0:
+        if FLAGS.log_train_steps != 0 and i%FLAGS.log_train_steps == 0:
             metrics.train(data_sources, data_target, global_step, t)
 
         # Evaluate every log_val_steps but also at the last step
         validation_accuracy_source = None
         validation_accuracy_target = None
-        if i%FLAGS.log_val_steps == 0 or i == FLAGS.steps:
+        if (FLAGS.log_val_steps != 0 and i%FLAGS.log_val_steps == 0) \
+                or i == FLAGS.steps:
             validation_accuracy_source, validation_accuracy_target \
                 = metrics.test(global_step)
 
@@ -162,12 +163,13 @@ def main(argv):
         # a new validation accuracy. If this is better than the previous best
         # model, we need to make a new checkpoint so we can restore from this
         # step with the best accuracy.
-        if i%FLAGS.model_steps == 0 or validation_accuracy_source is not None:
+        if (FLAGS.model_steps != 0 and i%FLAGS.model_steps == 0) \
+                or validation_accuracy_source is not None:
             checkpoint_manager.save(int(global_step-1),
                 validation_accuracy_source, validation_accuracy_target)
 
         # Plots
-        if i%FLAGS.log_plots_steps == 0:
+        if FLAGS.log_plots_steps != 0 and i%FLAGS.log_plots_steps == 0:
             metrics.plots(global_step)
 
     # We're done -- used for hyperparameter tuning
