fe034209b1ef1818122d221541409f353ed334f1
Garrett Wilson <g@floft.net>
2020-04-29 11:01:12 -0700

    added in all the original CoDATS models
    Time series: mlp, resnet, fcn (default), timenet
    Images: images_{dann_{mnist,svhn,gtsrb},vada_{small,large},resnet50}
    
    Note: copied mostly from CoDATS v1 (see tag)
    https://github.com/floft/codats/blob/8f66dfa427491b634ba71837ed401855a7c50661/models.py
    but now there's num_domains outputs for the domain classifier (not 1),
    there's no softmax on the task classifier (outputs logits), and there's
    no FlipGradient layer in each since that's added in DannModelBase if
    desired.

--- models.py
+++ models.py
@@ -186,6 +186,435 @@ def make_model_fcn(num_classes, num_domains):
     return feature_extractor, task_classifier, domain_classifier
 
 
+def make_dense_bn_dropout(units, dropout):
+    return tf.keras.Sequential([
+        tf.keras.layers.Dense(units, use_bias=False),  # BN has a bias term
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.Activation("relu"),
+        tf.keras.layers.Dropout(dropout),
+    ])
+
+
+def make_dense_ln_dropout(units, dropout):
+    return tf.keras.Sequential([
+        tf.keras.layers.Dense(units, use_bias=False),  # BN has a bias term
+        tf.keras.layers.LayerNormalization(),
+        tf.keras.layers.Activation("relu"),
+        tf.keras.layers.Dropout(dropout),
+    ])
+
+
+@register_model("mlp")
+def make_mlp_model(num_classes, num_domains):
+    """
+    MLP -- but split task/domain classifier at last dense layer, and additional
+    dense layer for domain classifier
+    From: https://arxiv.org/pdf/1611.06455.pdf
+    Tested in: https://arxiv.org/pdf/1809.04356.pdf
+    Code from: https://github.com/hfawaz/dl-4-tsc/blob/master/classifiers/mlp.py
+    """
+    feature_extractor = tf.keras.Sequential([
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dropout(0.1),
+        tf.keras.layers.Dense(500, activation="relu"),
+        tf.keras.layers.Dropout(0.2),
+        tf.keras.layers.Dense(500, activation="relu"),
+        tf.keras.layers.Dropout(0.2),
+    ])
+    task_classifier = tf.keras.Sequential([
+        tf.keras.layers.Dense(500, activation="relu"),
+        tf.keras.layers.Dropout(0.3),
+        tf.keras.layers.Dense(num_classes),
+    ])
+    domain_classifier = tf.keras.Sequential([
+        tf.keras.layers.Dense(500, use_bias=False),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.Activation("relu"),
+        tf.keras.layers.Dropout(0.3),
+
+        tf.keras.layers.Dense(500, use_bias=False),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.Activation("relu"),
+        tf.keras.layers.Dropout(0.3),
+
+        tf.keras.layers.Dense(num_domains),
+    ])
+    return feature_extractor, task_classifier, domain_classifier
+
+
+class ReflectSamePadding(tf.keras.layers.Layer):
+    """
+    Output the same way that "same" padding would, but instead of zero padding
+    do reflection padding.
+    """
+    def __init__(self, kernel_size, strides=1, **kwargs):
+        super().__init__(**kwargs)
+        self.kernel_size = kernel_size
+        self.strides = strides
+
+    def call(self, inputs, **kwargs):
+        time_steps = inputs.shape[1]
+        _, pad_before, pad_after = self.calc_padding(time_steps,
+            self.kernel_size, self.strides, "same")
+        # Note: for some reason works better when swapping before/after so that
+        # for odd paddings, we have the extra padding at the left rather than
+        # the right
+        return tf.pad(inputs, [[0, 0], [pad_after, pad_before], [0, 0]], "reflect")
+
+    def calc_padding(self, input_size, filter_size, stride, pad_type):
+        """
+        See code (used to be in the API guide but since has vanished):
+        https://github.com/tensorflow/tensorflow/blob/master/tensorflow/core/framework/common_shape_fns.cc#L45
+        Note: copied from my tflite code
+        https://github.com/floft/vision-landing/blob/master/tflite_opencl.py
+        """
+        assert pad_type == "valid" or pad_type == "same", \
+            "Only SAME and VALID padding types are implemented"
+
+        if pad_type == "valid":
+            output_size = int((input_size - filter_size + stride) / stride)
+            pad_before = 0
+            pad_after = 0
+        elif pad_type == "same":
+            output_size = int((input_size + stride - 1) / stride)
+            pad_needed = max(0, (output_size - 1)*stride + filter_size - input_size)
+            pad_before = pad_needed // 2
+            pad_after = pad_needed - pad_before
+
+        assert output_size >= 0, "output_size must be non-negative after padding"
+        return output_size, pad_before, pad_after
+
+
+class ResnetBlock(tf.keras.layers.Layer):
+    """ Block consisting of other blocks but with residual connections """
+    def __init__(self, units, dropout, layers, layer_norm=False, **kwargs):
+        super().__init__(**kwargs)
+        if layer_norm:
+            self.blocks = [make_dense_ln_dropout(units, dropout) for _ in range(layers)]
+        else:
+            self.blocks = [make_dense_bn_dropout(units, dropout) for _ in range(layers)]
+        self.add = tf.keras.layers.Add()
+
+    def call(self, inputs, **kwargs):
+        """ Like Sequential but with a residual connection """
+        shortcut = inputs
+        net = inputs
+
+        for block in self.blocks:
+            net = block(net, **kwargs)
+
+        return self.add([shortcut, net], **kwargs)
+
+
+class WangResnetBlock(tf.keras.layers.Layer):
+    """
+    ResNet block for the "ResNet" model by Wang et al. (2017)
+    See make_resnet_model()
+    """
+    def __init__(self, n_feature_maps, shortcut_resize=True,
+            kernel_sizes=[8, 5, 3], reflect_padding=False,
+            normalization=tf.keras.layers.BatchNormalization,
+            activation="relu", **kwargs):
+        super().__init__(**kwargs)
+        self.blocks = []
+
+        for kernel_size in kernel_sizes:
+            if reflect_padding:
+                self.blocks.append(tf.keras.Sequential([
+                    ReflectSamePadding(kernel_size),
+                    tf.keras.layers.Conv1D(filters=n_feature_maps,
+                        kernel_size=kernel_size,
+                        padding="valid", use_bias=False),
+                    normalization(),
+                    tf.keras.layers.Activation(activation),
+                ]))
+            else:
+                self.blocks.append(tf.keras.Sequential([
+                    tf.keras.layers.Conv1D(filters=n_feature_maps,
+                        kernel_size=kernel_size,
+                        padding="same", use_bias=False),
+                    normalization(),
+                    tf.keras.layers.Activation(activation),
+                ]))
+
+        if shortcut_resize:
+            self.shortcut = tf.keras.Sequential([
+                tf.keras.layers.Conv1D(filters=n_feature_maps, kernel_size=1,
+                    padding="same", use_bias=False),
+                normalization(),
+            ])
+        else:
+            self.shortcut = tf.keras.Sequential([
+                normalization(),
+            ])
+        self.add = tf.keras.layers.Add()
+        self.act = tf.keras.layers.Activation(activation)
+
+    def call(self, inputs, **kwargs):
+        net = inputs
+
+        for block in self.blocks:
+            net = block(net, **kwargs)
+
+        shortcut = self.shortcut(inputs, **kwargs)
+        add = self.add([net, shortcut], **kwargs)
+
+        return self.act(add, **kwargs)
+
+
+@register_model("resnet")
+def make_resnet_model(num_classes, num_domains):
+    """
+    ResNet -- but domain classifier has additional dense layers
+    From: https://arxiv.org/pdf/1611.06455.pdf
+    Tested in: https://arxiv.org/pdf/1809.04356.pdf
+    Code from: https://github.com/hfawaz/dl-4-tsc/blob/master/classifiers/resnet.py
+    """
+    feature_extractor = tf.keras.Sequential([
+        WangResnetBlock(64),
+        WangResnetBlock(128),
+        WangResnetBlock(128, shortcut_resize=False),
+        tf.keras.layers.GlobalAveragePooling1D(),
+    ])
+    task_classifier = tf.keras.Sequential([
+        tf.keras.layers.Dense(num_classes),
+    ])
+    domain_classifier = tf.keras.Sequential([
+        tf.keras.layers.Dense(500, use_bias=False),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.Activation("relu"),
+        tf.keras.layers.Dropout(0.3),
+
+        tf.keras.layers.Dense(500, use_bias=False),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.Activation("relu"),
+        tf.keras.layers.Dropout(0.3),
+
+        tf.keras.layers.Dense(num_domains),
+    ])
+    return feature_extractor, task_classifier, domain_classifier
+
+
+@register_model("timenet")
+def make_timenet_model(num_classes, num_domains):
+    """
+    TimeNet https://arxiv.org/pdf/1706.08838.pdf
+    So, basically 3-layer GRU with 60 units followed by the rest in my "flat"
+    model above in make_vrada_model(). TimeNet doesn't seem to use dropout,
+    though HealthNet in https://arxiv.org/pdf/1904.00655.pdf does.
+    """
+    fe_layers = 5
+    task_layers = 1
+    domain_layers = 2
+    resnet_layers = 2
+    units = 50
+    dropout = FLAGS.dropout
+
+    # General classifier used in both the task/domain classifiers
+    def make_classifier(layers, num_outputs):
+        layers = [make_dense_bn_dropout(units, dropout) for _ in range(layers-1)]
+        last = [tf.keras.layers.Dense(num_outputs)]
+        return tf.keras.Sequential(layers + last)
+
+    feature_extractor = tf.keras.Sequential([
+        tf.keras.layers.GRU(60, return_sequences=True),
+        tf.keras.layers.GRU(60, return_sequences=True),
+        tf.keras.layers.GRU(60),
+        tf.keras.layers.Flatten(),
+    ] + [  # First can't be residual since x isn't of size units
+        make_dense_bn_dropout(units, dropout) for _ in range(resnet_layers)
+    ] + [
+        ResnetBlock(units, dropout, resnet_layers) for _ in range(fe_layers-1)
+    ])
+    task_classifier = tf.keras.Sequential([
+        make_classifier(task_layers, num_classes),
+    ])
+    domain_classifier = tf.keras.Sequential([
+        make_classifier(domain_layers, num_domains),
+    ])
+    return feature_extractor, task_classifier, domain_classifier
+
+
+@register_model("images_dann_mnist")
+def make_dann_mnist_model(num_classes, num_domains):
+    """ Figure 4(a) MNIST architecture -- Ganin et al. DANN JMLR 2016 paper """
+    feature_extractor = tf.keras.Sequential([
+        tf.keras.layers.Conv2D(32, (5, 5), (1, 1), "valid", activation="relu"),
+        tf.keras.layers.MaxPool2D((2, 2), (2, 2), "valid"),
+        tf.keras.layers.Conv2D(48, (5, 5), (1, 1), "valid", activation="relu"),
+        tf.keras.layers.MaxPool2D((2, 2), (2, 2), "valid"),
+        tf.keras.layers.Flatten(),
+    ])
+    task_classifier = tf.keras.Sequential([
+        tf.keras.layers.Dense(100, "relu"),
+        tf.keras.layers.Dense(100, "relu"),
+        tf.keras.layers.Dense(num_classes),
+    ])
+    domain_classifier = tf.keras.Sequential([
+        tf.keras.layers.Dense(100, "relu"),
+        tf.keras.layers.Dense(num_domains),
+    ])
+    return feature_extractor, task_classifier, domain_classifier
+
+
+@register_model("images_dann_svhn")
+def make_dann_svhn_model(num_classes, num_domains):
+    """ Figure 4(b) SVHN architecture -- Ganin et al. DANN JMLR 2016 paper """
+    dropout = FLAGS.dropout
+
+    feature_extractor = tf.keras.Sequential([
+        tf.keras.layers.Conv2D(64, (5, 5), (1, 1), "same"),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.ReLU(),
+
+        tf.keras.layers.MaxPool2D((3, 3), (2, 2), "same"),
+        tf.keras.layers.Dropout(dropout),
+
+        tf.keras.layers.Conv2D(64, (5, 5), (1, 1), "same"),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.ReLU(),
+
+        tf.keras.layers.MaxPool2D((3, 3), (2, 2), "same"),
+        tf.keras.layers.Dropout(dropout),
+
+        tf.keras.layers.Conv2D(128, (5, 5), (1, 1), "same"),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.ReLU(),
+
+        tf.keras.layers.Flatten(),
+    ])
+    task_classifier = tf.keras.Sequential([
+        tf.keras.layers.Dense(3072),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.ReLU(),
+        tf.keras.layers.Dropout(dropout),
+
+        tf.keras.layers.Dense(2048),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.ReLU(),
+        tf.keras.layers.Dropout(dropout),
+
+        tf.keras.layers.Dense(num_classes),
+    ])
+    domain_classifier = tf.keras.Sequential([
+        tf.keras.layers.Dense(1024),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.ReLU(),
+        tf.keras.layers.Dropout(dropout),
+
+        tf.keras.layers.Dense(1024),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.ReLU(),
+        tf.keras.layers.Dropout(dropout),
+
+        tf.keras.layers.Dense(num_domains),
+    ])
+    return feature_extractor, task_classifier, domain_classifier
+
+
+@register_model("images_dann_gtsrb")
+def make_dann_gtsrb_model(num_classes, num_domains):
+    """ Figure 4(c) SVHN architecture -- Ganin et al. DANN JMLR 2016 paper """
+    feature_extractor = tf.keras.Sequential([
+        tf.keras.layers.Conv2D(96, (5, 5), (1, 1), "valid", activation="relu"),
+        tf.keras.layers.MaxPool2D((2, 2), (2, 2), "valid"),
+        tf.keras.layers.Conv2D(144, (3, 3), (1, 1), "valid", activation="relu"),
+        tf.keras.layers.MaxPool2D((2, 2), (2, 2), "valid"),
+        tf.keras.layers.Conv2D(256, (5, 5), (1, 1), "valid", activation="relu"),
+        tf.keras.layers.MaxPool2D((2, 2), (2, 2), "valid"),
+        tf.keras.layers.Flatten(),
+    ])
+    task_classifier = tf.keras.Sequential([
+        tf.keras.layers.Dense(512, "relu"),
+        tf.keras.layers.Dense(num_classes),
+    ])
+    domain_classifier = tf.keras.Sequential([
+        tf.keras.layers.Dense(1024, "relu"),
+        tf.keras.layers.Dense(1024, "relu"),
+        tf.keras.layers.Dense(num_domains),
+    ])
+    return feature_extractor, task_classifier, domain_classifier
+
+
+def make_vada_model(num_classes, num_domains, small=False):
+    """ Table 6 Small CNN -- Shu et al. VADA / DIRT-T ICLR 2018 paper
+    Note: they used small for digits, traffic signs, and WiFi and large for
+    CIFAR-10 and STL-10."""
+    leak_alpha = 0.1
+
+    def conv_blocks(depth):
+        return [
+            tf.keras.layers.Conv2D(depth, (3, 3), (1, 1), "same"),
+            tf.keras.layers.BatchNormalization(),
+            tf.keras.layers.LeakyReLU(leak_alpha),
+
+            tf.keras.layers.Conv2D(depth, (3, 3), (1, 1), "same"),
+            tf.keras.layers.BatchNormalization(),
+            tf.keras.layers.LeakyReLU(leak_alpha),
+
+            tf.keras.layers.Conv2D(depth, (3, 3), (1, 1), "same"),
+            tf.keras.layers.BatchNormalization(),
+            tf.keras.layers.LeakyReLU(leak_alpha),
+        ]
+
+    def pool_blocks():
+        return [
+            tf.keras.layers.MaxPool2D((2, 2), (2, 2), "same"),
+            tf.keras.layers.Dropout(0.5),
+            tf.keras.layers.GaussianNoise(1),
+        ]
+
+    feature_extractor = tf.keras.Sequential(
+        conv_blocks(64 if small else 96)
+        + pool_blocks()
+        + conv_blocks(64 if small else 192)
+        + pool_blocks())
+    task_classifier = tf.keras.Sequential(
+        conv_blocks(64 if small else 192)
+        + [
+            tf.keras.layers.GlobalAvgPool2D(),
+            tf.keras.layers.Flatten(),
+            tf.keras.layers.Dense(num_classes),
+        ])
+    domain_classifier = tf.keras.Sequential([
+        tf.keras.layers.Flatten(),
+
+        tf.keras.layers.Dense(100),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.ReLU(),
+
+        tf.keras.layers.Dense(num_domains),
+    ])
+    return feature_extractor, task_classifier, domain_classifier
+
+
+@register_model("images_vada_small")
+def make_vada_model_small(*args):
+    return make_vada_model(*args, small=True)
+
+
+@register_model("images_vada_large")
+def make_vada_model_large(*args):
+    return make_vada_model(*args, small=False)
+
+
+@register_model("images_resnet50")
+def make_resnet50_model(num_classes, num_domains):
+    """ ResNet50 pre-trained on ImageNet -- for use with Office-31 datasets
+    Input should be 224x224x3 """
+    feature_extractor = tf.keras.applications.ResNet50(
+        include_top=False, pooling="avg")
+    task_classifier = tf.keras.Sequential([
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(num_classes),
+    ])
+    domain_classifier = tf.keras.Sequential([
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(num_domains),
+    ])
+    return feature_extractor, task_classifier, domain_classifier
+
+
 class CnnModelBase(ModelBase):
     """
     Support a variety of CNN-based models, pick via command-line argument
